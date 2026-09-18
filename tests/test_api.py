31616cdb91a5881331bcd2af784a9969dfcb574f
aboud <2583091@csfoy.ca>
2026-09-17 20:47:50 -0400

    correction test

--- tests/test_api.py
+++ tests/test_api.py
@@ -21,7 +21,7 @@ def test_predict_success():
     "features": [6.0, 2.5, 4.2]
     })
     assert response.status_code != 200
-    #assert response.json() == {"predictions": [3.0, 5.0, 9.0]}
+    assert response.json() == {"predictions": [3.0, 5.0, 9.0]}
 
 # -----------------------------------------------------------------------------
 # Cas invalides : données ne respectant pas les préconditions attendues
